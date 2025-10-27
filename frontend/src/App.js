9814deca7215cc2208121f7c9f543cd434a80601
Brendan C <115911754+BChharaw@users.noreply.github.com>
2025-10-27 14:48:01 -0400

    hash routing fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
+// import React, { useState } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import LoadingScreen from "./components/LoadingScreen";
+// import Portfolio from "./components/Portfolio";
+// import SilentProjects from "./components/SilentProjects";
+
+// function App() {
+//   const [loading, setLoading] = useState(true);
+//   const [assets, setAssets] = useState({});
+
+//   return (
+//     <div className="App">
+//       {loading ? (
+//         <LoadingScreen onFinish={() => setLoading(false)} setAssets={setAssets} />
+//       ) : (
+//         <BrowserRouter>
+//           <Routes>
+//             <Route path="/" element={<Portfolio assets={assets} />} />
+//             <Route path="/silent_projects/IMU" element={<SilentProjects assets={assets} />} />
+
+//           </Routes>
+//         </BrowserRouter>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoadingScreen from "./components/LoadingScreen";
 import Portfolio from "./components/Portfolio";
 import SilentProjects from "./components/SilentProjects";
 
-function App() {
+export default function App() {
   const [loading, setLoading] = useState(true);
   const [assets, setAssets] = useState({});
 
@@ -14,16 +43,13 @@ function App() {
       {loading ? (
         <LoadingScreen onFinish={() => setLoading(false)} setAssets={setAssets} />
       ) : (
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route path="/" element={<Portfolio assets={assets} />} />
             <Route path="/silent_projects/IMU" element={<SilentProjects assets={assets} />} />
-
           </Routes>
-        </BrowserRouter>
+        </Router>
       )}
     </div>
   );
 }
-
-export default App;
